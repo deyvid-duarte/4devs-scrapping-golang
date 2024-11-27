1fcf2a2d130e9491981d383d4d99a45e2d161ec6
Deyvid Duarte <deyviddc@gmail.com>
2024-11-26 22:44:07 -0300

    feat: adicao de scrapper para o cnpj do 4devs

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	CPF "4devs-scrapping/src/Cpf"
+	"4devs-scrapping/src/CNPJ"
+	"4devs-scrapping/src/CPF"
 	"fmt"
 )
 
 func main() {
-	cpf := CPF.Generate(true, "SP")
-	fmt.Print("O CPF é: ", cpf)
+	cpf := CPF.Generate(false, "SP")
+	cnpj := CNPJ.Generate(false)
+	fmt.Println("O CPF é:", cpf)
+	fmt.Println("O CNPJ é:", cnpj)
 }
--- src/Cnpj/cnpj.go
+++ src/Cnpj/cnpj.go
@@ -1,7 +1,29 @@
 package CNPJ
 
-import "fmt"
+import (
+	"io"
+	"net/http"
+	"net/url"
+)
 
-func Generate() {
-	fmt.Println("Gerando CNPJ")
+func Generate(withMask bool) string {
+	var withMaskString string;
+	if withMask {
+		withMaskString = "S"
+	} else {
+		withMaskString = "N"
+	}
+
+	formData := url.Values{}
+	formData.Set("acao", "gerar_cnpj")
+	formData.Set("pontuacao", withMaskString)
+
+	resp, err := http.PostForm("https://www.4devs.com.br/ferramentas_online.php", formData)
+	if err != nil {
+		panic("Erro ao gerar o CNPJ: " + err.Error())
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	return string(body)
 }
